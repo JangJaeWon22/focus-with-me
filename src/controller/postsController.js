ae4464e26552917fcf4546eb699a4573006d8f70
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-10-26 12:02:22 +0900

    서연제 GET/POST 통신, DB 생성작업 완료

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,18 +1,55 @@
+const { Post } = require("../models");
+const { Op } = require("sequelize");
+
 module.exports = {
   getPosts: (req, res) => {
-    console.log("GET posts");
     return res.send({});
   },
-  postPosts: (req, res) => {
-    console.log("POST posts");
-    return res.send({});
+  postPosts: async (req, res) => {
+    const {
+      // headers: { token },
+      body: {
+        imageCover,
+        title,
+        categorySpace,
+        categoryStudyMates,
+        categoryInterests,
+        imageContents,
+        textContent,
+        youtubeUrl,
+      },
+    } = req;
+    const userId = 237237420;
+    const date = new Date();
+    const post = {
+      userId,
+      imageCover,
+      title,
+      categoryInterests,
+      categorySpace,
+      categoryStudyMates,
+      imageContents,
+      textContent,
+      youtubeUrl,
+      date,
+    };
+
+    await Post.create(post);
+
+    return res.status(200).send({ message: "게시물 작성 성공!" });
   },
   putPosts: (req, res) => {
     console.log("PUT posts");
     return res.send({});
   },
-  deletePosts: (req, res) => {
-    console.log("DELETE posts");
+  deletePosts: async (req, res) => {
+    const { postId } = req.params;
+    await Post.destory({
+      where: {
+        postId,
+      },
+    });
+
     return res.send({});
   },
 };
