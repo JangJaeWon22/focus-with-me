4281c2b13972264068cb361982eed507bedc8c64
jjangjae <nick2866@naver.com>
2021-10-29 14:46:33 +0900

    db 관련 env 재설정

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: "root",
-    password: "1234",
+    password: process.env.DB_TEST_PW,
     database: process.env.DB_NAME,
     host: "127.0.0.1",
     dialect: "mysql",
