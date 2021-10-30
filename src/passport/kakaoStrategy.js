6f34397bbfa1fb77a3939243dca04813a15904ee
jjangjae <nick2866@naver.com>
2021-10-30 17:42:18 +0900

    소셜 로그인 콜백주소 URL 맞추기

--- src/passport/kakaoStrategy.js
+++ src/passport/kakaoStrategy.js
@@ -8,7 +8,7 @@ module.exports = () => {
     new KakaoStrategy(
       {
         clientID: process.env.KAKAO_ID,
-        callbackURL: "/auth/kakao/callback",
+        callbackURL: "http://localhost:3000/api/kakao/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         console.log("kakao profile", profile);
