4a68291fb048b6454bb86f99965da5af9b0ce1c7
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-11-01 20:17:37 +0900

    서연제 쿼리 기반 필터 조회 테스트 중

--- src/middlewares/filter.js
+++ src/middlewares/filter.js
@@ -1,14 +1,25 @@
-const { Post, User } = require("../models");
+const { Op } = require("sequelize");
+const { Post, User, Like } = require("../models");
 
 const filter = (req, res, next) => {
   const { searchMode } = req.query;
   searchMode === "main"
     ? next()
     : (async () => {
-        console.log("여기는 필터할 미들웨어");
-        req.queryResult = { message: "쿼리 결과 : 필터" };
-        const posts = await Post.findAll({});
-        console.log(posts);
+        const { categorySpace, categoryInterest, categoryStudyMate } =
+          req.query;
+
+        let where = [];
+        if (categoryInterest) where.push({ categoryInterest });
+        if (categorySpace) where.push({ categorySpace });
+        if (categoryStudyMate) where.push({ categoryStudyMate });
+        console.log(where);
+        const posts = await Post.findAll({
+          where: {
+            [Op.and]: where, // assign the "where" array here
+          },
+          limit: 10,
+        });
         req.posts = posts;
         next();
       })();
@@ -18,8 +29,17 @@ const main = (req, res, next) => {
   const { searchMode } = req.query;
   searchMode === "filter"
     ? next()
-    : (() => {
+    : (async () => {
+        const posts = await Post.findAll({
+          include: [
+            {
+              model: Like,
+            },
+          ],
+        });
+        console.log("하하하", posts);
         console.log("여기는 메인 미들웨어");
+        req.posts = posts;
         req.queryResult = { message: "쿼리 결과 : 메인" };
         next();
       })();
