62cda8e999e1d27ba49678c130e8aaeb4d86d02e
jjangjae <nick2866@naver.com>
2021-11-02 21:47:02 +0900

    좋아요 라우터 생성

--- src/middlewares/filter.js
+++ src/middlewares/filter.js
@@ -1,15 +1,5 @@
-const { Op, Sequelize } = require("sequelize");
+const { Op, sequelize, Sequelize } = require("../models");
 const { Post, User, Like } = require("../models");
-// const { Sequelize, QueryTypes } = require("sequelize");
-// const sequelize = new Sequelize("focus", "root", process.env.DB_PASSWORD, {
-//   host: "localhost",
-//   dialect: "mysql" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000,
-//   },
-// });
 const filter = (req, res, next) => {
   const { searchMode } = req.query;
   searchMode === "main"
@@ -45,25 +35,17 @@ const main = (req, res, next) => {
     : (async () => {
         // 여기서 해야 할 일
         // 전체 게시물을 좋아요와 join 뒤에 좋아요 순으로 정렬, limit 10개만
-        const sqlQuery = `
+        const postQuery = `
         SELECT Posts.*, COUNT(Likes.postId) AS cnt
         FROM Posts
         JOIN Likes On Posts.postId = Likes.postId
         GROUP BY Posts.postId
-        ORDER BY cnt ASC;`;
+        ORDER BY cnt DESC;`;
 
-        const posts = await Post.findAll({
-          attributes: {
-            include: [[Sequelize.fn("COUNT", "Like.postId"), "cnt"]],
-          },
-          include: [
-            {
-              model: Like,
-              attributes: [],
-            },
-          ],
-          group: ["postId"],
+        const posts = await sequelize.query(postQuery, {
+          type: Sequelize.QueryTypes.SELECT,
         });
+
         console.log(posts);
         console.log("여기는 메인 미들웨어");
         req.posts = posts;
