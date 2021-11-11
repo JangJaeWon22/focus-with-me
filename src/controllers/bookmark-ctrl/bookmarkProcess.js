b3907449d084e6822ce4f6acf048fba12d6bd092
jjangjae <nick2866@naver.com>
2021-11-12 00:49:30 +0900

    댓글 조회 시 로그인 유저에 대해 댓글 좋아요 boolean 값과 좋아요 카운트 수 전달

--- src/controllers/bookmark-ctrl/bookmarkProcess.js
+++ src/controllers/bookmark-ctrl/bookmarkProcess.js
@@ -3,22 +3,29 @@ const { Bookmark } = require("../../models");
 const bookmarkProcess = {
   createbookmark: async (req, res) => {
     try {
+      // params로 postId 받아옴
       const { postId } = req.params;
+      // 미들웨어를 통해 userId 받아옴
       const { userId } = res.locals.user;
+      // 해당 postId와 userId를 가진 bookmark를 가져와 보자
       const bookmark = await Bookmark.findOne({ where: { postId, userId } });
+      // 날짜 생성
       const date = new Date();
+      // 북마크가 없을 때
       if (!bookmark) {
-        const bookmarkedPost = await Bookmark.create({
+        // 북마크 추가 실행
+        await Bookmark.create({
           postId,
           userId,
           bookmark,
           date,
         });
+        // 성공 응답값 200 및 로그인 유저가 북마크 했으면 true값을 보내어 프론트에서 state 바로 적용.
         res.status(200).send({
           isBookmarked: true,
-          bookmarkedPost,
           message: "북마크를 완료 했습니다.",
         });
+        // 이미 북마크를 함.
       } else {
         res.status(400).send({
           message: "북마크를 이미 했습니다.",
@@ -26,6 +33,7 @@ const bookmarkProcess = {
       }
     } catch (err) {
       console.error(err);
+      // try 구문에서 발생한 오류 catch해서 메세지 전송.
       res.status(500).send({
         message: "알 수 없는 문제가 발생했습니다. 잠시 후 다시 시도해주세요.",
       });
@@ -34,30 +42,37 @@ const bookmarkProcess = {
 
   deleteBookmark: async (req, res) => {
     try {
+      // params로 postId 값 가져옴
       const { postId } = req.params;
+      // 사용자 인증 미들웨어로 userId 값 받아옴
       const { userId } = res.locals.user;
-      console.log(userId);
+      // 해당 postId와 userId를 가진 bookmark를 가져와 보자
       const bookmark = await Bookmark.findOne({ where: { postId, userId } });
+      // 북마크가 있을 때
       if (bookmark) {
+        // 북마크의 userId가 로그인한 userId가 같을 경우
         if (bookmark.userId === userId) {
-          await Bookmark.destroy({
-            where: { postId, userId },
-          });
+          //해당 북마크 db 삭제
+          await bookmark.destroy();
+          // 성공 응답값 200 및 로그인 유저가 북마크 취소하면 false값을 보내어 프론트에서 state 바로 적용.
           res.status(200).send({
             isBookmarked: false,
             message: "북마크를 취소 했습니다.",
           });
+          // 북마크의 userId가 로그인한 userId가 다를 경우의 응답 값
         } else {
           res.status(400).send({
             message: "작성자가 아닙니다.",
           });
         }
+        // 북마크가 없을 때의 응닶 값
       } else {
         res.status(400).send({
           message: "북마크 한 정보를 찾을 수 없습니다.",
         });
       }
     } catch (error) {
+      // try 구문에서 발생한 오류 catch해서 메세지 전송.
       console.error(error);
       res.status(500).send({
         message: "알 수 없는 문제가 발생했습니다. 잠시 후 다시 시도해주세요.",
--- src/controllers/likes-ctrl/cmtslikeControl.js
+++ src/controllers/likes-ctrl/cmtslikeControl.js
@@ -5,43 +5,46 @@ const commentsLikeFunc = {
   likeExist: async (req, res) => {
     try {
       // params에 postId, commentId 객체
-      const { postId, commentId } = req.params; 
+      const { postId, commentId } = req.params;
       // res.locals.user 는 미들웨어인 loginOnly에서 값을 가져와 userId에 할당한다
-      const { userId } = res.locals.user; 
-      
+      const { userId } = res.locals.user;
+
       // user가 이미 좋아요를 했는지 확인하기 위해 CommentLike 를 조회
       const likeCmt = await CommentLike.findOne({
         where: { postId, commentId, userId },
       });
 
-      // 댓글을 좋아한 적이 없는 초기화 상태에서 algorithm 시작 
-      if(!likeCmt){
+      // 댓글을 좋아한 적이 없는 초기화 상태에서 algorithm 시작
+      if (!likeCmt) {
         const date = new Date();
         // 댓글 좋아요 생성
         await CommentLike.create({
-          postId, userId, commentId, date,
+          postId,
+          userId,
+          commentId,
+          date,
         });
         // 댓글 카운터 기능 - 댓글의 수를 세어주기 위해서 만들어짐
         const likeCount = await CommentLike.count({
           where: { commentId, postId },
         });
-        
+
         // 댓글 상태 메세지 기능
         return res.status(200).send({
           isLiked: true,
           likeCount,
           message: "댓글에 좋아요를 눌렀습니다.",
-        })
+        });
       } else {
-        return res.status(400).send({ 
+        return res.status(400).send({
           message: "좋아요를 이미 눌렀어요.",
         });
       }
     } catch (err) {
       console.log(err);
-      return res.status(500).send({ 
+      return res.status(500).send({
         message: "댓글 좋아요 기능에 문제가 생겼습니다.",
-      })
+      });
     }
   },
   // 댓글 좋아요 취소 생성
@@ -56,37 +59,38 @@ const commentsLikeFunc = {
 
       // 댓글 좋아요 있는지 확인한 상태에서 algorithm 시작
       // 댓글 좋아요 있으면 delete 요청
-      if (likeCmt) {  
+      if (likeCmt) {
         await CommentLike.destroy({
-            where: { postId, commentId, userId }  
+          where: { postId, commentId, userId },
         });
 
         // 댓글의 수를 세어주는 기능
-        // 댓글 카운터 기능 없이도 좋아요 추가와 삭제의 기능 작동이 잘 되지만, 
+        // 댓글 카운터 기능 없이도 좋아요 추가와 삭제의 기능 작동이 잘 되지만,
         // commentId 와 postId 가 있어야 n-1 이 가능하고, userId 가 포함될 경우, 0 이 되기 때문에, userId를 지워주는 것이 맞음
         const likeCount = await CommentLike.count({
           where: { commentId, postId },
         });
-      
+
         // 댓글 상태 메세지 기능
         return res.status(200).send({
           isLiked: false,
           likeCount,
-          message: "댓글 좋아요 해제 완료! 다음번에 더 좋은 댓글 좋아요 클릭해주세요.",
+          message:
+            "댓글 좋아요 해제 완료! 다음번에 더 좋은 댓글 좋아요 클릭해주세요.",
         });
       } else {
-        return res.status(400).send({ 
-          message: "좋아요를 한 상태에서만 가능한 기능입니다." 
+        return res.status(400).send({
+          message: "좋아요를 한 상태에서만 가능한 기능입니다.",
         });
       }
     } catch (err) {
       console.log(err);
-      return res.status(500).send({ 
-        message: "관리자에게 문의해주세요." 
+      return res.status(500).send({
+        message: "관리자에게 문의해주세요.",
       });
     }
   },
-}
+};
 
 // 댓글 라우터 연결
-module.exports = { commentsLikeFunc }
+module.exports = { commentsLikeFunc };
