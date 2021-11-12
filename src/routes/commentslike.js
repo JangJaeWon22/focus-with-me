e5edcced977bd2607f0c7e80a082372a4f7f4247
Jenny9197 <passionnhope0110@gmail.com>
2021-11-12 13:44:21 +0900

    documentation updated

--- src/routes/commentslike.js
+++ src/routes/commentslike.js
@@ -1,10 +1,11 @@
-// Insert express
+// express 는 함수나 객체 또는 변수가 할당된 모듈이다
+// require() 는 JS 라이브러리/파일이다
 const express = require("express");
-// Insert router
+// router를 세팅 하기 위해 필요하다
 const router = express.Router();
-// Insert controller function
+// 댓글 좋아요를 위해 컨트롤러를 적용했다
 const cmtslikeCtrl = require("../controllers/likes-ctrl/cmtsLikeControl");
-// Insert middleware function but available only when logged in
+// 로그인 했을 때만 이용 가능한 미들웨어다
 const { logInOnly } = require("../middlewares/passport-auth");
 
 // 댓글 좋아요 추가
