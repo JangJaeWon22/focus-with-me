57ef55fe7869145cf6dc7ada3b0c01b422d6627c
Jenny9197 <passionnhope0110@gmail.com>
2021-11-20 01:44:36 +0900

    index.js update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
-
+const nodemailer = require("nodemailer");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+router.post("/nodemailerTest", function(req, res, next){
+  let email = req.body.email;
+  
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user:'test@gmail.com',
+      password:'test'
+    }
+  });
+
+  let mailOptions = {
+    from: 'test@gmail.com',
+    to: email,
+    subject: 'sending email using node.js',
+    text: 'challenge yourself'
+  };
+  
+  transporter.sendMail
+});
 module.exports = router;
