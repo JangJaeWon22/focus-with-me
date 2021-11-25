d12d6200e3b1bd1aa29e5e889699e2c4b587015a
Jenny9197 <passionnhope0110@gmail.com>
2021-11-25 20:34:57 +0900

    test

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const job = schedule.scheduleJob("0 0 0 * * *", async () => {
   await emptyTempS3();
   console.log("temp 폴더 삭제");
 });
-console.log("hi");
+
 app.listen(port, () => {
   logger.info(`
   ${port} 포트에서 서버가 가동되었습니다.😄😄
