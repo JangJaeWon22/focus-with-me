39c5898ffbf0474ceb8fddcfea8363b8a4681f77
jjangjae <nick2866@naver.com>
2021-11-29 23:53:16 +0900

    장재원 test

--- src/routes/bookmark.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var router = express.Router();
-var passport_auth_1 = require("../middlewares/passport-auth");
-var bookmarkProcess_1 = require("../controllers/bookmarkProcess");
-router.post("/bookmarks/:postId", passport_auth_1.logInOnly, bookmarkProcess_1["default"].createbookmark);
-router["delete"]("/bookmarks/:postId", passport_auth_1.logInOnly, bookmarkProcess_1["default"].deleteBookmark);
-exports["default"] = router;
